e5e102a0eeaedc311ca41242daadf8e86788bacf
Yash Jaiswal <125455407+mr-yash-jaiswal045@users.noreply.github.com>
2023-11-19 20:58:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    var li = document.createElement("li");
+    li.textContent = taskInput.value;
+    li.innerHTML += '<button onclick="removeTask(this)">Remove</button>';
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
+
+function removeTask(button) {
+    var li = button.parentNode;
+    li.parentNode.removeChild(li);
+}
